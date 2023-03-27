bba35483d8f1f1069c1226c0a4c37d59c15c5afd
aminld <amin.llaven2@gmail.com>
2023-03-27 17:09:16 -0600

    se agrega primer ejercicio de arreglos

---
+++ JS-04 - Arrays and Loops/js/main.js
@@ -0,0 +1,15 @@
+console.log("+++++++++++Ejercicio 1.1++++++++++");
+let ordenFila = ["Sofía","David","Juan"];
+console.log(ordenFila);
+console.log(ordenFila.sort());
+
+console.log("+++++++++++Ejercicio 1.2++++++++++");
+let nombresFila = ["Sofía","David","Juan"];
+nombresFila.push("Sara","Augustin");
+nombresFila.shift();
+console.log(nombresFila);
+
+console.log("+++++++++++Ejercicio 1.3++++++++++");
+nombresFila.splice(1, 0, "Renata");
+nombresFila.push("Elena")
+console.log(nombresFila);
